182e91526436678a753d03197b65aba0bfb9123a
JuwanMG <juwangilliam@yahoo.com>
2017-06-15 18:24:51 -0400

    Mission Demolition Update
    Completed code for the projectile follow camera

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    static public FollowCam S;
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _____________;
+    public GameObject poi;
+    public float camZ;
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    // Use this for initialization
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (poi == null) return;
+
+        Vector3 destination = poi.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+        
+	}
+}
