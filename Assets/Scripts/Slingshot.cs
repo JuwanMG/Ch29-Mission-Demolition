dd10e2b171ed4ba924da0078837e7050f11e2d7f
JuwanMG <juwangilliam@yahoo.com>
2017-06-15 20:09:03 -0400

    Mission Demolition Update
    implemented code for the projectile line

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    static public Slingshot S;
 
     public GameObject launchPoint;
     public GameObject prefabProjectile;
@@ -14,6 +15,7 @@ public class Slingshot : MonoBehaviour {
 
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
